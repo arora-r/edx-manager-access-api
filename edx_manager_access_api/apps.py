6b633e8bc00304695a52739c067ae1cc3b4b9882
Rohit <rohit.arora1@ibm.com>
2020-12-18 15:12:40 -0500

    Files generated by edx-cookiecutter command

---
+++ edx_manager_access_api/apps.py
@@ -0,0 +1,13 @@
+"""
+edx_manager_access_api Django application initialization.
+"""
+
+from django.apps import AppConfig
+
+
+class EdxManagerAccessApiConfig(AppConfig):
+    """
+    Configuration for the edx_manager_access_api Django application.
+    """
+
+    name = 'edx_manager_access_api'
---
+++ tests/test_models.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+"""
+Tests for the `edx-manager-access-api` models module.
+"""
